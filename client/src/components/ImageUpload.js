e147faf4ac817b0467a616b63d47ea4625173754
sergey <sbarchshevskiy@gmail.com>
2021-05-07 11:25:04 -0400

    create react app and setup firebase backend storage

---
+++ client/src/components/ImageUpload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function ImageUpload() {
+  return(
+
+    <div>
+      <form>
+        <input type="file"/>
+      </form>
+    </div>
+  )
+}
---
+++ client/src/firebaseConfig/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app'
+import 'firebase/storage';
+import 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAJw7txItQiwsPA4KsVg7-SpYH-2efrLo",
+  authDomain: "small-image-repo.firebaseapp.com",
+  projectId: "small-image-repo",
+  storageBucket: "small-image-repo.appspot.com",
+  messagingSenderId: "588127984440",
+  appId: "1:588127984440:web:f0742426d432e3fa13ffcd"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const backendStorage = firebase.storage();
+const backendFirestore = firebase.firestore();
+
+export {backendFirestore, backendStorage};
