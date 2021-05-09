02c238d319d9737c2b2167b4106933a860fca29a
sergey <sbarchshevskiy@gmail.com>
2021-05-09 17:30:04 -0400

    implement delete function **bug**

--- client/src/components/DisplayImages.js
+++ client/src/components/DisplayImages.js
@@ -1,17 +1,35 @@
-import React from 'react';
 import useFirestore from "../hooks/useFirestore";
+import firebase from "firebase";
+import React, { useState } from 'react';
+import firestore from "firebase";
+import {backendFirestore, backendStorage} from "../firebaseConfig/config";
 
 
-export default function DisplayImages() {
+export default function DisplayImages({file}) {
+
+  const [images, setImages] = useState([])
 
   const { imageFiles } = useFirestore('pics');
   console.log('images: ',imageFiles)
+
+  const deleteImage = () => {
+    const db = firebase.firestore();
+    db.collection('pics').doc(imageFiles.id).delete()
+      .then(() => {
+        setImages((prev) =>
+        prev.filter((element) => element.id !== imageFiles.id))
+      }).catch((err) => {
+      console.log('error', err)
+    })
+  }
 
   return(
     <div className="image-display">
       { imageFiles && imageFiles.map(imageFile => (
         <div className="image-display-table" key={imageFile.id}>
           <img src={imageFile.url} alt="image" />
+          <div><button className="image-delete-button" onClick={deleteImage}>delete</button></div>
+
         </div>
       ))}
     </div>
